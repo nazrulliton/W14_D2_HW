fb855ff5b2bc51f9c58d25788aa9ec6a6d1c29e1
naz_liton <nazrulliton@icloud.com>
2020-01-28 15:52:32 +0000

    render created

--- src/containers/MusicContainer.js
+++ src/containers/MusicContainer.js
@@ -1,4 +1,4 @@
-import React,  from 'react';
+import React  from 'react';
 
 class MusicContainer extends React.Component {
     constructor(props){
@@ -12,13 +12,26 @@ componentDidMount(){
     const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
     fetch(url)
     .then(res => res.json())
-    .then(songs => this.setState({songs: songs}))
+    .then(songs => this.setState({songs: songs.feed.entry}))
     .catch(err => console.error);
 }
 
+render(){
+    const selectedSong = this.state.songs.find(song => song.id.attributes['im:id'] === this.state.selectedImId)
+
+    return (
+        <div> 
+            <h2>Music Container</h2>
+            {/* <MusicList/>
+            <Song/> */}
+        </div>
+    )
+}
 
 
 
 
 
 }
+
+export default MusicContainer;
